0b6e9eab89fbd1250f9000001800d83a94a77370
Matt Clay <matt@mystile.com>
2019-02-12 12:18:34 -0800

    Update scripts, requirements and packages:
    - Add Python 3.8 Alpha 1.
    - Improve output and maintainability of requirements script.
    - Update and freeze requirements.

--- freeze.sh
+++ freeze.sh
@@ -16,4 +16,12 @@ for version in ${versions}; do
     docker run -it default-test-container-freezer "pip${version}" freeze -qqq --disable-pip-version-check | tr -d '\r' > "freeze/${version}.txt"
 done
 
+echo "Fixing bad frozen versions ..."
+
+for version in ${versions}; do
+    echo "Fixing frozen versions for Python ${version} ..."
+    sed 's/^solidfire-sdk-python==1.5.0.87$/solidfire-sdk-python==1.5.0.87.post1  # .post1 required for successful install/' < "freeze/${version}.txt" > /tmp/freeze.txt
+    mv /tmp/freeze.txt "freeze/${version}.txt"
+done
+
 echo "Completed freeze process. You can build the container now."
