270ea61f5f61ec32cf45fe7db04704b6a1b815bb
Jordan Borean <jborean93@gmail.com>
2019-03-19 15:32:13 +1000

    Add custom PSSA rule and include pypsrp in container

--- requirements/sanity.ps1
+++ requirements/sanity.ps1
@@ -6,3 +6,6 @@ $ErrorActionPreference = "Stop"
 
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name PSScriptAnalyzer -RequiredVersion 1.16.1
+
+# Used to setup the PSCustomUseLiteralPath rule for PSSA
+Install-Module -Name PSSA-PSCustomUseLiteralPath -RequiredVersion 0.1.1
